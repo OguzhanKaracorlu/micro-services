d05632db2124f225abfea259e371dc3fb1faa655
oguzhan.karacorlu <oguzhankaracorlu@gmail.com>
2022-10-18 11:42:44 +0300

    limits-service inital commit

---
+++ micro-services/limits-service/src/test/java/com/oguzhankaracorlu/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oguzhankaracorlu.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
